d44113a02d5427f4dd5a72e3b125e0a918ccc363
jakob_michael <jakob_luge@mailbox.org>
2023-08-19 10:51:32 +0000

    Update 4 files

--- exercise-tracker/src/index.js
+++ exercise-tracker/src/index.js
@@ -1,104 +1,112 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/src/views/index.html");
 });
 
-const bodyParser = require('body-parser');
-const dns = require('dns');
-const url = require('url');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const dns = require("dns");
+const url = require("url");
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const userSchema = new Schema({
   username: { type: String, required: true },
-  log: [{
-    description: { type: String, required: true },
-    duration: { type: Number, required: true },
-    date: { type: Date }
-  }]
+  log: [
+    {
+      description: { type: String, required: true },
+      duration: { type: Number, required: true },
+      date: { type: Date },
+    },
+  ],
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
-app.post('/api/users', (req, res) => {
+app.post("/api/users", (req, res) => {
   const { username } = req.body;
 
   User.findOne({ username })
-    .then(user => {
-
-      if (user) return res.send('Username already taken');
+    .then((user) => {
+      if (user) return res.send("Username already taken");
 
       const newUser = new User({ username });
-      newUser.save()
-        .then(user => res.json({ username: user.username, _id: user._id }))
-        .catch(err => res.send('Error saving user to database'));
-    }
-    ).catch(err => console.log(err));
+      newUser
+        .save()
+        .then((user) => res.json({ username: user.username, _id: user._id }))
+        .catch((err) => res.send("Error saving user to database"));
+    })
+    .catch((err) => console.log(err));
 });
 
-app.get('/api/users', (req, res) => {
+app.get("/api/users", (req, res) => {
   User.find({})
-    .then(users => res.json(users))
-    .catch(err => res.send('Error getting users from database'));
+    .then((users) => res.json(users))
+    .catch((err) => res.send("Error getting users from database"));
 });
 
-app.post('/api/users/:_id/exercises', (req, res) => {
+app.post("/api/users/:_id/exercises", (req, res) => {
   const { _id } = req.params;
   const { description, duration, date } = req.body;
 
   User.findById(_id)
-    .then(user => {
-      if (!user) return res.send('Unknown userId');
+    .then((user) => {
+      if (!user) return res.send("Unknown userId");
 
       const newExercise = {
         description,
         duration: Number(duration),
-        date: date ? new Date(date) : new Date()
+        date: date ? new Date(date) : new Date(),
       };
 
       user.log.push(newExercise);
-      user.save()
-        .then(user => res.json({
-          _id: user._id,
-          username: user.username,
-          date: newExercise.date.toDateString(),
-          duration: newExercise.duration,
-          description: newExercise.description
-        }))
-        .catch(err => res.send('Error saving exercise to database'));
-    }
-    ).catch(err => console.log(err));
+      user
+        .save()
+        .then((user) =>
+          res.json({
+            _id: user._id,
+            username: user.username,
+            date: newExercise.date.toDateString(),
+            duration: newExercise.duration,
+            description: newExercise.description,
+          })
+        )
+        .catch((err) => res.send("Error saving exercise to database"));
+    })
+    .catch((err) => console.log(err));
 });
 
-app.get('/api/users/:_id/logs', (req, res) => {
+app.get("/api/users/:_id/logs", (req, res) => {
   const { _id } = req.params;
   const { from, to, limit } = req.query;
 
   User.findById(_id)
-    .then(user => {
-      if (!user) return res.send('Unknown userId');
+    .then((user) => {
+      if (!user) return res.send("Unknown userId");
 
       let log = user.log;
 
       if (from) {
         const fromDate = new Date(from);
-        log = log.filter(exercise => exercise.date >= fromDate);
+        log = log.filter((exercise) => exercise.date >= fromDate);
       }
 
       if (to) {
         const toDate = new Date(to);
-        log = log.filter(exercise => exercise.date <= toDate);
+        log = log.filter((exercise) => exercise.date <= toDate);
       }
 
       if (limit) {
@@ -109,17 +117,16 @@ app.get('/api/users/:_id/logs', (req, res) => {
         _id: user._id,
         username: user.username,
         count: log.length,
-        log: log.map(exercise => ({
+        log: log.map((exercise) => ({
           description: exercise.description,
           duration: exercise.duration,
-          date: exercise.date.toDateString()
-        }))
+          date: exercise.date.toDateString(),
+        })),
       });
-    }
-    ).catch(err => console.log(err));
+    })
+    .catch((err) => console.log(err));
 });
 
-
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
