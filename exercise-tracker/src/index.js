494e967e6feb448639a28399bed0a791f920ed3a
Jakob <jakob_luge@mailbox.org>
2023-08-19 12:12:01 +0200

    add exercise tracker

---
+++ exercise-tracker/src/index.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+
+app.use(cors())
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+});
+
+const bodyParser = require('body-parser');
+const dns = require('dns');
+const url = require('url');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  log: [{
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    date: { type: Date }
+  }]
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.post('/api/users', (req, res) => {
+  const { username } = req.body;
+
+  User.findOne({ username })
+    .then(user => {
+
+      if (user) return res.send('Username already taken');
+
+      const newUser = new User({ username });
+      newUser.save()
+        .then(user => res.json({ username: user.username, _id: user._id }))
+        .catch(err => res.send('Error saving user to database'));
+    }
+    ).catch(err => console.log(err));
+});
+
+app.get('/api/users', (req, res) => {
+  User.find({})
+    .then(users => res.json(users))
+    .catch(err => res.send('Error getting users from database'));
+});
+
+app.post('/api/users/:_id/exercises', (req, res) => {
+  const { _id } = req.params;
+  const { description, duration, date } = req.body;
+
+  User.findById(_id)
+    .then(user => {
+      if (!user) return res.send('Unknown userId');
+
+      const newExercise = {
+        description,
+        duration: Number(duration),
+        date: date ? new Date(date) : new Date()
+      };
+
+      user.log.push(newExercise);
+      user.save()
+        .then(user => res.json({
+          _id: user._id,
+          username: user.username,
+          date: newExercise.date.toDateString(),
+          duration: newExercise.duration,
+          description: newExercise.description
+        }))
+        .catch(err => res.send('Error saving exercise to database'));
+    }
+    ).catch(err => console.log(err));
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
